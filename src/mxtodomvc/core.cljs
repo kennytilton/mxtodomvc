734132f2e8abd08e1d8dc44d0b35cbdafdce0596
Kenneth Tilton <kentilton@gmail.com>
2018-07-30 10:17:38 -0400

    better starting code

--- src/mxtodomvc/core.cljs
+++ src/mxtodomvc/core.cljs
@@ -1,27 +1,23 @@
-
 (ns ^:figwheel-hooks mxtodomvc.core
-  (:require [goog.dom :as dom]
-            [clojure.string :as str]
-            [tiltontec.cell.core :refer-macros [cF cFonce] :refer [cI]]
-            [tiltontec.model.core
-             :refer [matrix mx-par <mget <mget mset!> mset!>
-                     mxi-find mxu-find-name] :as md]
-            [mxweb.gen
-             :refer [evt-tag target-value]
-             :refer-macros [h1 input div span]]
-            [mxweb.html :refer [tag-dom-create]]))
+  (:require
+    [goog.dom :as dom]
+    [clojure.string :as str]
+    [tiltontec.cell.core :refer-macros [cFonce]]
+    [tiltontec.model.core :refer [<mget] :as md]
+    [mxweb.gen :refer-macros [h1 div]]
+    [mxweb.html :refer [tag-dom-create]]))
 
 (enable-console-print!)
 
-(let [root (dom/getElement "tagroot")
-      app-matrix (md/make
-                   ::hellomx
-                   :mx-dom (cFonce (md/with-par me
-                                                [(div {}
-                                                      (h1 {} "hello,
-                                                      Matrix"))])))
-      app-dom (tag-dom-create
-                (md/<mget app-matrix :mx-dom))]
+(defn matrix-build []
+  (md/make
+    :mx-dom (cFonce (md/with-par me
+                      [(div {}
+                         (h1 {} "hello, Matrix"))]))))
 
+(let [root (dom/getElement "tagroot")
+      app-matrix (matrix-build)]
   (set! (.-innerHTML root) nil)
-  (dom/appendChild root app-dom))
+  (dom/appendChild root
+    (tag-dom-create
+      (<mget app-matrix :mx-dom))))
