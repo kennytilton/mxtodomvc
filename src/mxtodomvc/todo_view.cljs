86bf97177427c420ed2d2bcbffd40d6647b20dfb
Kenneth Tilton <kentilton@gmail.com>
2018-08-04 18:38:20 -0400

    XHR rizing

--- src/mxtodomvc/todo_view.cljs
+++ src/mxtodomvc/todo_view.cljs
@@ -1,15 +1,59 @@
 (ns mxtodomvc.todo-view
   (:require
     [tiltontec.util.core :as util]
-    [tiltontec.cell.core :refer-macros [cF]]
-    [tiltontec.model.core :as md]
+    [tiltontec.cell.base :refer [unbound]]
+    [tiltontec.cell.observer :refer-macros [fn-obs]]
+    [tiltontec.cell.core :refer-macros [cF cF+]]
+    [tiltontec.cell.evaluate :refer [not-to-be]]
+
+    [tiltontec.model.core :refer [matrix mx-par <mget ;; mset!> mswap!>
+                                  ;;fget mxi-find mxu-find-type
+                                  ] :as md]
+
+    [mxxhr.core
+     :refer [make-xhr xhr-response]]
+
     [mxweb.gen
-     :refer-macros [label li div input button]]
+     :refer-macros [label li div input button span]]
     [mxweb.html :as mxweb]
+
     [mxtodomvc.todo
      :refer [td-title td-created
              td-completed td-delete!
-             td-id td-toggle-completed!]]))
+             td-id td-toggle-completed!]]
+    [cljs.pprint :as pp]
+    [clojure.string :as str]))
+
+;;; -----------------------------------------------------------
+;;; --- adverse events ----------------------------------------
+
+(defn de-whitespace [s]
+  (str/replace s #"\s" ""))
+
+(def ae-by-brand "https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:~(~a~)&limit=3")
+
+(defn ae-explorer [todo]
+  (button {:class   "li-show"
+           :style   (cF (str "display:"
+                          (or (when-let [xhr (<mget me :ae)]
+                                (let [aes (xhr-response xhr)]
+                                  (when (= 200 (:status aes))
+                                    "block")))
+                            "none")))
+           :onclick #(js/alert "Feature not yet implemented.")}
+
+    {:ae (cF+ [:obs (fn-obs
+                      ;; we use an observer to GC old XHRs
+                      (when-not (or (= old unbound) (nil? old))
+                        (not-to-be old)))]
+           (when true ;; (<mget (mxweb/mxu-find-class me "ae-autocheck") :on?)
+             (make-xhr (pp/cl-format nil ae-by-brand
+                         (js/encodeURIComponent (td-title todo)))
+               {:name name :send? true})))}
+
+    (span {:style "font-size:0.7em;margin:2px;margin-top:0;vertical-align:top"}
+      "View Adverse Events")))
+
 
 (defn todo-list-item [todo]
   ;; the structure below, and importantly its CSS, was authored
@@ -35,6 +79,8 @@
               :onclick     #(td-toggle-completed! todo)})
 
       (label (td-title todo))
+
+      (ae-explorer todo)
 
       (button {:class   "destroy"
                ;; we actually have an td-delete! to hide the action, but
