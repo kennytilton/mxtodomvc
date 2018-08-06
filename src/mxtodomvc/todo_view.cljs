d39ae9e3d075d4ecb6c9730fde6c895787ff0cd1
Kenneth Tilton <kentilton@gmail.com>
2018-08-06 07:06:03 -0400

    todo now editable

--- src/mxtodomvc/todo_view.cljs
+++ src/mxtodomvc/todo_view.cljs
@@ -1,12 +1,16 @@
 (ns mxtodomvc.todo-view
   (:require
+    [goog.dom :as dom]
+    [goog.dom.classlist :as classlist]
+    [goog.dom.forms :as form]
+
     [tiltontec.util.core :as util]
     [tiltontec.cell.base :refer [unbound]]
     [tiltontec.cell.observer :refer-macros [fn-obs]]
     [tiltontec.cell.core :refer-macros [cF cF+]]
     [tiltontec.cell.evaluate :refer [not-to-be]]
 
-    [tiltontec.model.core :refer [matrix mx-par <mget       ;; mset!> mswap!>
+    [tiltontec.model.core :refer [matrix mx-par <mget mset!> ;; mswap!>
                                   ;;fget mxi-find mxu-find-type
                                   ] :as md]
 
@@ -78,6 +82,26 @@
                       :undecided))}
     "warning"))
 
+(defn todo-edit [e todo edit-commited?]
+  (let [edt-dom (.-target e)
+        li-dom (dom/getAncestorByTagNameAndClass edt-dom "li")]
+
+    (when (classlist/contains li-dom "editing")
+      (let [title (str/trim (form/getValue edt-dom))
+            stop-editing #(classlist/remove li-dom "editing")]
+        (cond
+          edit-commited?
+          (do
+            (stop-editing)                                  ;; has to go first cuz a blur event will sneak in
+            (if (= title "")
+              (td-delete! todo)
+              (mset!> todo :title title)))
+
+          (= (.-key e) "Escape")
+          ;; this could leave the input field with mid-edit garbage, but
+          ;; that gets initialized correctly when starting editing
+          (stop-editing))))))
+
 (defn todo-list-item [todo]
   ;; the structure below, and importantly its CSS, was authored
   ;; by the developers of the TodoMVC exercise. Nice!
@@ -101,7 +125,13 @@
               ;; td-toggle-completed! expands to an mset!> of the JS epoch or nil
               :onclick     #(td-toggle-completed! todo)})
 
-      (label (td-title todo))
+      (label {:ondblclick #(let [li-dom (dom/getAncestorByTagNameAndClass
+                                          (.-target %) "li")
+                                 edt-dom (dom/getElementByClass
+                                           "edit" li-dom)]
+                             (classlist/add li-dom "editing")
+                             (mxweb/input-editing-start edt-dom (td-title todo)))}
+        (td-title todo))
 
       (adverse-event-checker todo)
 
@@ -109,4 +139,8 @@
                ;; we actually have an td-delete! to hide the action, but
                ;; this is a tutorial so let's show the action and use mset!.
                ;; btw, yes, we extend here the spec to support logical deletion
-               :onclick #(md/mset!> todo :deleted (util/now))}))))
+               :onclick #(md/mset!> todo :deleted (util/now))}))
+
+    (input {:class     "edit"
+            :onblur    #(todo-edit % todo true)
+            :onkeydown #(todo-edit % todo (= (.-key %) "Enter"))})))
