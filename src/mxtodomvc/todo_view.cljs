3ada5039b0f53370778572e57b8f3f9e31329297
Kenneth Tilton <kentilton@gmail.com>
2018-08-25 10:57:04 -0400

    restore xhr

--- src/mxtodomvc/todo_view.cljs
+++ src/mxtodomvc/todo_view.cljs
@@ -45,7 +45,6 @@
 (defn de-whitespace [s]
   (str/replace s #"\s" ""))
 
-#_
 (defn adverse-event-checker [todo]
   (i
     {:class   "aes material-icons"
@@ -125,7 +124,7 @@
                              (mxweb/input-editing-start edt-dom (td-title todo)))}
         (td-title todo))
 
-      ;;(adverse-event-checker todo)
+      (adverse-event-checker todo)
 
       (button {:class   "destroy"
                ;; we actually have an td-delete! to hide the action, but
